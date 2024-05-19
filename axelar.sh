b8715dea71df611f4bded007c063fbe8059da7d2
godekggs <163100903+godekggs@users.noreply.github.com>
2024-05-19 19:33:55 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-e4a25962-769c-43a9-b12d-2ec5a9a7ae41/"
 
 OUTPUT_DIR="logs"
 
